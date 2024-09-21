172e539778410403c64d37ea0f7190992121520d
Gabriela Ramires <engenheira.ramires@gmail.com>
2024-09-21 16:01:25 -0300

    pronto

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,14 +1,31 @@
 <template>
   <div id='home'>
     <div class="chamativo">
-      <h1 class="texto-chamativo">Crie sua <span class='destaque'>campanha</span> em poucos minutos!</h1>
-      <p class="frase-marketing">A plataforma completa para você criar e gerenciar suas campanhas com praticidade e
-        facilidade.</p>
+      <h3>Sobre a história</h3>
+      <p class="frase-marketing">
+        Renata, uma jovem introspectiva, muda-se para Aracaju e enfrenta a ansiedade do colégio novo. Lá, ela conhece Luíza, uma colega que desperta nela uma conexão inesperada. A história explora os desafios da adaptação, amizade e autodescoberta.</p>
     </div>
 
     <div class='botoes'>
-      <a href="#" class='btn-escuro'>Quero criar agora mesmo!</a>
-      <a href="#" class='btn-branco'>Saber mais</a>
+      <a href="/about" class='btn-escuro'>Começar leitura</a>
+    </div>
+
+
+    <div class="cards">
+      <h3>Últimos capítulos</h3>
+      <div class="card">
+      <img src="../assets/img/capas/capitulo1.jpg" alt="Capítulo 1">
+      <div class="overlay">
+      <h5>Capítulo 1</h5>
+      <h4>Um novo ciclo</h4>      
+      </div>
+    </div>
+    <div class="botoes">
+      <a href="/about" class='btn-branco'>Acessar</a>      
+    </div>
+
+
+
     </div>
 
   </div>
