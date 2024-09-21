9fed0c2d17d67338737e60bfc8633e5eb27d8fbe
Gabriela Ramires <engenheira.ramires@gmail.com>
2024-09-21 16:35:11 -0300

    colocando link na capa

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,26 +3,32 @@
     <div class="chamativo">
       <h3>Sobre a história</h3>
       <p class="frase-marketing">
-        Renata, uma jovem introspectiva, muda-se para Aracaju e enfrenta a ansiedade do colégio novo. Lá, ela conhece Luíza, uma colega que desperta nela uma conexão inesperada. A história explora os desafios da adaptação, amizade e autodescoberta.</p>
+        Renata, uma jovem introspectiva, muda-se para Aracaju e enfrenta a ansiedade do colégio novo. Lá, ela conhece
+        Luíza, uma colega que desperta nela uma conexão inesperada. A história explora os desafios da adaptação, amizade
+        e autodescoberta.</p>
     </div>
 
     <div class='botoes'>
-      <router-link to="/about" class='btn-escuro'>Começar leitura</router-link>     
+      <router-link to="/about" class='btn-escuro'>Começar leitura</router-link>
     </div>
 
 
     <div class="cards">
       <h3>Últimos capítulos</h3>
-      <div class="card">
-      <img src="../assets/img/capas/capitulo1.jpg" alt="Capítulo 1">
-      <div class="overlay">
-      <h5>Capítulo 1</h5>
-      <h4>Um novo ciclo</h4>      
+      <router-link to="/about">
+        <div class="card">
+            <img src="../assets/img/capas/capitulo1.jpg" alt="Capítulo 1">
+          <div class="overlay">
+            <h5>Capítulo 1</h5>
+            <h4>Um novo ciclo</h4>
+          </div>
       </div>
-    </div>
-    <div class="botoes">
-      <router-link to="/about" class='btn-branco'>Acessar</router-link>   
-    </div>
+      </router-link>
+
+
+      <div class="botoes">
+        <router-link to="/about" class='btn-branco'>Acessar</router-link>
+      </div>
 
 
 
