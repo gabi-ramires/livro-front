5535e43d042cceed17e35aee9d7039fb68e2b756
Gabriela Ramires <engenheira.ramires@gmail.com>
2024-09-21 19:19:50 -0300

    foi

--- dist/index.php
+++ dist/index.php
@@ -1,6 +1,17 @@
 <?php
 
 echo '<pre>';
+session_start();
+
+$session_id = session_id();
+$ip = $_SERVER['REMOTE_ADDR'];
+$dispositivo = $_SERVER['HTTP_USER_AGENT'];
+
+var_dump($_SESSION);
 var_dump($_SERVER);
+
+
+
+
 
 require_once('app.html');
