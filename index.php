3ab807037f586a2d04755bfd413286d37650aa79
Gabriela Ramires <engenheira.ramires@gmail.com>
2024-09-21 21:13:22 -0300

    foi

--- index.php
+++ index.php
@@ -63,9 +63,9 @@ window.addEventListener('beforeunload', function() {
     console.log('saiuuu')
     <?php
 // Prepara e executa a atualização
-$sql = "UPDATE historico_acessos SET data_fim = ? WHERE data_inicio = ?";
+$sql = "UPDATE historico_acessos SET data_fim = ? WHERE session_id = ? AND data_fim is null" ;
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("ss", date('Y-m-d H:i:s'), $data);
+$stmt->bind_param("ss", date('Y-m-d H:i:s'), $session_id);
     ?>
 
 });
